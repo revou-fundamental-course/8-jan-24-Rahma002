79833edefecfa5cf9395225a9f303c0ec671123a
Rahmawati <rahma002wati@gmail.com>
2024-01-17 19:18:46 +0700

    Final adding formula to the result

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,7 +1,10 @@
 function hitung() {
   S = document.getElementById("S").value;
+  document.getElementById("Sisi1").innerHTML = S;
+  document.getElementById("Sisi2").innerHTML = S;
   Luas = S * S;
   document.getElementById("Luas").value = Luas;
+  document.getElementById("Kel").innerHTML = S;
   Keliling = 4 * S;
   document.getElementById("Keliling").value = Keliling;
 }
@@ -10,4 +13,8 @@ function reset() {
   document.getElementById("S").value = "";
   document.getElementById("Luas").value = "";
   document.getElementById("Keliling").value = "";
+  document.getElementById("Sisi1").innerHTML = "S";
+  document.getElementById("Sisi2").innerHTML = "S";
+  document.getElementById("Kel").innerHTML = "S";
 }
+
